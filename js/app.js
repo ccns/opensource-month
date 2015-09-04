8de752f9bc3d5b49d1f1f5e00dbc1be1eb3c9d5f
DanielChen <team6612@gmail.com>
2015-09-04 20:03:48 +0800

    remove angular and add activity area

--- js/app.js
+++
@@ -1,8 +0,0 @@
-var app = angular.module('myApp',['ngRoute']);
-
-app.config(function($routeProvider) {
-  $routeProvider
-  .when('/', {
-    templateUrl: 'views/indexview.html'
-  }).otherwise({ redirectTo: '/' });
-});
